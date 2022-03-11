d00ff210e89b60daaf9b35a9a1d03fe891b7a999
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-03-11 15:49:13 +0700

    Update CORS

--- src/main/java/com/rmit/onyx2/repository/WorkspaceRepository.java
+++ src/main/java/com/rmit/onyx2/repository/WorkspaceRepository.java
@@ -5,13 +5,18 @@ import com.rmit.onyx2.model.Workspace;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
     @Modifying
     @Query("delete from Workspace w where w.workspaceId=:workspaceId")
     void deleteById(Long workspaceId);
+
+    @Query("select w from Workspace w left join User u on u.userId=:userId")
+    List<Workspace> findAllByUserId(Long userId);
 }
