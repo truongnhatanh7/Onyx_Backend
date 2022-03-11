0704811a0c75a3ba4920c01a7bd90ef4ed329e77
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-03-11 10:40:49 +0700

    Add delete

--- src/main/java/com/rmit/onyx2/repository/TaskRepository.java
+++ src/main/java/com/rmit/onyx2/repository/TaskRepository.java
@@ -2,8 +2,13 @@ package com.rmit.onyx2.repository;
 
 import com.rmit.onyx2.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    @Modifying
+    @Query("delete from Task t where t.taskId=:taskId")
+    void deleteById(Long taskId);
 }
