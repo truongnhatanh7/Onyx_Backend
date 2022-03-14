de8a3f0064e93726d4d57bb924be73ee892dda83
nhat117 <s3878174@rmit.edu.vn>
2022-03-14 11:29:49 +0700

    Add Edit the user

--- src/main/java/com/rmit/onyx2/controller/UserController.java
+++ src/main/java/com/rmit/onyx2/controller/UserController.java
@@ -2,15 +2,12 @@ package com.rmit.onyx2.controller;
 
 import com.rmit.onyx2.model.User;
 import com.rmit.onyx2.model.UserDTO;
-import com.rmit.onyx2.model.Workspace;
-import com.rmit.onyx2.repository.UserRepository;
 import com.rmit.onyx2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -35,6 +32,13 @@ public class UserController {
         return userService.addUser(user);
     }
 
+    @PutMapping("")
+    @CrossOrigin(origins = "http://127.0.0.1:5500/")
+    public ResponseEntity<User> editUser(@RequestBody User user) {
+        return userService.editUser(user);
+    }
+
+
     @PostMapping("/add-workspace-for-user-by-id/{workspaceId}/{userId}")
     @CrossOrigin(origins = "http://127.0.0.1:5500/")
     public ResponseEntity<User> addWorkspaceForUserById(@PathVariable(name = "workspaceId") Long workspaceId, @PathVariable(name = "userId") Long userId) {
@@ -46,5 +50,4 @@ public class UserController {
     public void deleteUserById(@PathVariable(name = "userId") Long userId) {
         userService.deleteUserById(userId);
     }
-
 }
