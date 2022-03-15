50fe72961441b69407bfd7725ffeea3932ac8a4b
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-03-15 10:20:22 +0700

    Clean comment

--- src/main/java/com/rmit/onyx2/Onyx2Application.java
+++ src/main/java/com/rmit/onyx2/Onyx2Application.java
@@ -1,9 +1,5 @@
 package com.rmit.onyx2;
 
-import com.rmit.onyx2.model.User;
-import com.rmit.onyx2.model.Workspace;
-import com.rmit.onyx2.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,25 +11,6 @@ public class Onyx2Application implements CommandLineRunner {
         SpringApplication.run(Onyx2Application.class, args);
     }
 
-//    @Autowired
-//    private UserRepository userRepository;
-
     @Override
-    public void run(String... args) throws Exception {
-//        User nanh = new User();
-//        nanh.setName("Truong Nhat Anh");
-//        nanh.setUsername("truongnhatanhh");
-//        nanh.setPassword("123456");
-//
-//        Workspace sef = new Workspace();
-//        sef.setWorkspaceTitle("SEF");
-//
-//        Workspace enterprise = new Workspace();
-//        enterprise.setWorkspaceTitle("Enterprise");
-//
-//        nanh.getWorkspaces().add(sef);
-//        nanh.getWorkspaces().add(enterprise);
-//
-//        userRepository.save(nanh);
-    }
+    public void run(String... args) throws Exception {}
 }
