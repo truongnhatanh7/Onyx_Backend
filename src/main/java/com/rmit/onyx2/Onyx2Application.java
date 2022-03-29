977c89b0c89b47e7d84e498cc35b4e565938018b
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-03-29 13:31:56 +0700

    Update backend

--- src/main/java/com/rmit/onyx2/Onyx2Application.java
+++ src/main/java/com/rmit/onyx2/Onyx2Application.java
@@ -3,6 +3,7 @@ package com.rmit.onyx2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class Onyx2Application implements CommandLineRunner {
