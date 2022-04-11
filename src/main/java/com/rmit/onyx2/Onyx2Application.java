7aa17a6b397e5e826afb53f29a1c18922372a9cb
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-04-11 12:24:44 +0700

    Turn off OpenAPI

--- src/main/java/com/rmit/onyx2/Onyx2Application.java
+++ src/main/java/com/rmit/onyx2/Onyx2Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title="OnyxAPI", version = "1.0", description = "This is a backend for Onyx Task Management System"))
+//@OpenAPIDefinition(info = @Info(title="OnyxAPI", version = "1.0", description = "This is a backend for Onyx Task Management System"))
 @EnableAsync
 public class Onyx2Application implements CommandLineRunner {
     static final String url = "123";
--- src/main/java/com/rmit/onyx2/controller/TaskController.java
+++ src/main/java/com/rmit/onyx2/controller/TaskController.java
@@ -27,47 +27,47 @@ public class TaskController {
     }
 
     @GetMapping("/{listId}")
-    @Operation(
-            summary = "Get all task by list id",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Return a Set of all Task based on list ID",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = Task.class))),
-                    @ApiResponse(responseCode = "400", description = "Return Task",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = Task.class)))
-            }
-    )
+//    @Operation(
+//            summary = "Get all task by list id",
+//            responses = {
+//                    @ApiResponse(responseCode = "200", description = "Return a Set of all Task based on list ID",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = Task.class))),
+//                    @ApiResponse(responseCode = "400", description = "Return Task",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = Task.class)))
+//            }
+//    )
     public Set<Task> getAllTasksByListId(@PathVariable(name = "listId") Long listId) {
         return taskService.getAllTasksByListId(listId);
     }
 
     @PostMapping("/{listId}")
-    @Operation(
-            summary = "Add Task by list ID",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Return Added Task",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = Task.class))),
-                    @ApiResponse(responseCode = "400", description = "invalid response",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ObjectUtils.Null.class)))}
-    )
+//    @Operation(
+//            summary = "Add Task by list ID",
+//            responses = {
+//                    @ApiResponse(responseCode = "200", description = "Return Added Task",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = Task.class))),
+//                    @ApiResponse(responseCode = "400", description = "invalid response",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = ObjectUtils.Null.class)))}
+//    )
     public Task addTaskByListId(@PathVariable(name = "listId") Long listId, @org.springframework.web.bind.annotation.RequestBody Task task) {
         return taskService.addTaskByListId(listId, task);
     }
 
     @PutMapping("")
-    @Operation(
-            summary = "Edit task",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Edit task success fully",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ResponseEntity.class))),
-                    @ApiResponse(responseCode = "400", description = "invalid response",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ResponseEntity.class)))}
-    )
+//    @Operation(
+//            summary = "Edit task",
+//            responses = {
+//                    @ApiResponse(responseCode = "200", description = "Edit task success fully",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = ResponseEntity.class))),
+//                    @ApiResponse(responseCode = "400", description = "invalid response",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = ResponseEntity.class)))}
+//    )
     public ResponseEntity<Task> editTask(@org.springframework.web.bind.annotation.RequestBody Task task) {
         return taskService.editTask(task);
     }
@@ -79,33 +79,33 @@ public class TaskController {
 
     //To change list destination
     @PutMapping("/switchList/{destinationListId}")
-    @Operation(
-            summary = "Change the list destination",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Edit task successfully",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ResponseEntity.class))),
-                    @ApiResponse(responseCode = "400", description = "Edit task failed",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ResponseEntity.class)))
-            }
-    )
+//    @Operation(
+//            summary = "Change the list destination",
+//            responses = {
+//                    @ApiResponse(responseCode = "200", description = "Edit task successfully",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = ResponseEntity.class))),
+//                    @ApiResponse(responseCode = "400", description = "Edit task failed",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = ResponseEntity.class)))
+//            }
+//    )
     public ResponseEntity<Task> editTask(@RequestBody Task task, @PathVariable(name = "destinationListId") Long destinationListId) {
         return taskService.editTask(task, destinationListId);
     }
 
 
     @DeleteMapping("/{taskId}")
-    @Operation(
-            summary = "Change the list destination",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Delete user",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ObjectUtils.Null.class))),
-                    @ApiResponse(responseCode = "400", description = "Delete user",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ObjectUtils.Null.class)))}
-    )
+//    @Operation(
+//            summary = "Change the list destination",
+//            responses = {
+//                    @ApiResponse(responseCode = "200", description = "Delete user",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = ObjectUtils.Null.class))),
+//                    @ApiResponse(responseCode = "400", description = "Delete user",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = ObjectUtils.Null.class)))}
+//    )
     public void deleteTaskById(@PathVariable(name = "taskId") Long taskId) {
         taskService.deleteTaskById(taskId);
     }
