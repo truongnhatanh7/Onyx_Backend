f1be477ca3971e9f7045627a40919ed9178cbbf0
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-05-13 13:14:32 +0700

    asf

--- ProcFile
+++ ProcFile
@@ -1,1 +1,1 @@
-web: java -jar build/libs/rate-0.0.1-SNAPSHOT.jar
+web: java -Dspring.profiles.active=prod -Dserver.port=$PORT -jar build/libs/springboot.heroku.app-0.0.1-SNAPSHOT.jar
--- src/main/java/com/rmit/onyx2/model/User.java
+++ src/main/java/com/rmit/onyx2/model/User.java
@@ -1,16 +1,10 @@
 package com.rmit.onyx2.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "users")
